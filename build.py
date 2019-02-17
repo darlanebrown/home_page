9ebfad1f660d8d43e983e06fb19eb9d810a49ae7
Darla Brown <darlanebrown@gmail.com>
2019-02-16 16:50:48 -0800

    adding build.py file

--- build.py
+++ build.py
@@ -1,1 +1,12 @@
 print('hello world')
+top_template=open('templates/top.html').read()
+bottom_template=open('templates/bottom.html').read()
+
+middle_template=open('templates/content_index.html').read()
+index_html = top_template + middle_template + bottom_template
+open("docs/index.html",'w+').write(index_html)
+
+middle_template=open('templates/content_bio.html').read()
+bio_html = top_template + middle_template + bottom_template
+open("docs/bio.html",'w+').write(index_html)
+
